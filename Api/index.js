ca8d11868aa93a727fa72698d77bbaa45261441c
ObioraCJ <obiorachukwuemekaj@gmail.com>
2023-12-23 10:12:32 +0100

    Connected to Mongodb

--- Api/index.js
+++ Api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_ESTATE)
+.then(() => {
+    console.log('Mongodb is conected');
+}).catch((err) => {
+    console.log(err);
+}); 
 
 const app = express()
 
